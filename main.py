2142353d195fb3e25b5407e0f441b491ecb991fb
RyanB <ryanbeckel53@gmail.com>
2025-03-28 21:01:22 -0400

    Up Through CH2 L4

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("greetings boots")
+def get_book_text(filepath):
+	with open(filepath) as f:
+		return f.read()
+
+
+
+def main():
+	filepath = "/home/ryanb/workspacebootdev/bookbotproject/bookbot/books/frankenstein.txt"
+	book_text = get_book_text(filepath)
+	word_count = num_words(book_text)
+	word_count_message = f"{word_count} words found in the document"
+	print(word_count_message)
+	return (book_text)
+
+
+
+
+def num_words(book_text):
+	words = book_text.split()
+	word_count = len(words)
+	return word_count
+
+main()

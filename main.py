ca676924b59c8c92138d354906d9c3aca62a2ebe
RyanB <ryanbeckel53@gmail.com>
2025-04-02 19:15:16 -0400

    CH3 L2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 
 def get_book_text(filepath):
 	with open(filepath) as f:
@@ -9,14 +10,16 @@ def main():
 	from stats import character_count
 	from stats import num_words
 	from stats import organized_list
-	filepath = "/home/ryanb/workspacebootdev/bookbotproject/bookbot/books/frankenstein.txt"
+	if len(sys.argv) != 2:
+		print("Usage: python3 main.py <path_to_book>")
+	filepath = sys.argv[1]
 	book_text = get_book_text(filepath)
 	word_count = num_words(book_text)
 	word_count_message = f"{word_count} words found in the document"
 	count_of_characters = character_count(book_text)
 	organized_list = organized_list(book_text)
 	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
+	print("Analyzing book found at {filepath}...")
 	print("----------- Word Count ----------")
 	print(f"Found {word_count} total words")
 	print("--------- Character Count -------")
